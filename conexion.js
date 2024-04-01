f4149a823258cb84bf01324750a721f30fb90a7e
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2024-04-01 17:20:59 -0500

    Backend

---
+++ conexion.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: 'roundhouse.proxy.rlwy.net',
+    database: 'railway',
+    user: 'root',
+    password: 'mqvSPmcfofcuMlzKzpeCoXhXbdqqTobt',
+    port: 55998
+});
+
+conexion.connect(error => {
+    if (error) {
+        console.error('Error en la conexión a la base de datos:', error.message);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
+});
+
+module.exports = conexion;
